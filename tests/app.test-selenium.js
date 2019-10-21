bbc3005b73e13b20f3d63b43116d29f41035abab
getinthedamnbox <getinthedamnbox@gmail.com>
2019-10-21 11:29:16 -0400

    Worked on Selenium tests

--- tests/app.test-selenium.js
+++ tests/app.test-selenium.js
@@ -15,30 +15,79 @@ require('selenium-webdriver/chrome');
 const www = require('../bin/www');
 
 let driver = new Builder().forBrowser('chrome').build();
+const testName = 'testName';
+const testEmail = 'testEmail@test.com';
+const testPasswordCorrect = 'correctPassword';
+const testPasswordIncorrect = 'incorrectPassword';
 
 beforeAll(async () => {
   driver = await new Builder().forBrowser('chrome').build();
 }, 20000);
 
-afterAll((done) => {
-  www.server.shutdown(done);
-});
-
-async function getPage() {
-  driver.wait(until.urlIs('http://localhost:3000/login'));
-  await driver.get('http://localhost:3000/login');
-}
-
-beforeEach(async () => {
-  await getPage();
-});
-
 afterAll(async () => {
+  await www.server.shutdown();
   await driver.quit();
-}, 20000);
+});
 
 it('Loads the login page', async () => {
-  await driver.wait(until.elementLocated(By.id('heading')), 10000);
+  driver.wait(until.urlIs('http://localhost:3000/login'));
+  await driver.get('http://localhost:3000/login');
+
+  await driver.wait(until.elementLocated(By.id('heading')), 20000);
+  const heading = await driver.findElement(By.id('heading'));
+  expect(heading).not.toEqual(null);
+}, 60000);
+
+it('Goes to the registration page', async () => {
+  await driver.wait(until.elementLocated(By.id('register')), 20000);
+  const register = await driver.findElement(By.id('register'));
+  expect(register).not.toEqual(null);
+
+  await register.click();
+
+  driver.wait(until.urlIs('http://localhost:3000/register'));
+  await driver.get('http://localhost:3000/register');
+
+  await driver.wait(until.elementLocated(By.id('heading')), 20000);
+  const heading = await driver.findElement(By.id('heading'));
+  expect(heading).not.toEqual(null);
+}, 60000);
+
+it('Registers a new account', async () => {
+  driver.wait(until.urlIs('http://localhost:3000/register'));
+  await driver.get('http://localhost:3000/register');
+
+  await driver.wait(until.elementLocated(By.id('name')), 20000);
+  const name = await driver.findElement(By.id('name'));
+  expect(name).not.toEqual(null);
+
+  await driver.wait(until.elementLocated(By.id('email')), 20000);
+  const email = await driver.findElement(By.id('email'));
+  expect(email).not.toEqual(null);
+
+  await driver.wait(until.elementLocated(By.id('password')), 20000);
+  const password = await driver.findElement(By.id('password'));
+  expect(password).not.toEqual(null);
+
+  await driver.wait(until.elementLocated(By.id('submit')), 20000);
+  const submit = await driver.findElement(By.id('submit'));
+  expect(submit).not.toEqual(null);
+
+  await name.sendKeys('', Key.RETURN);
+  await name.sendKeys(testName, Key.RETURN);
+
+  await email.sendKeys('', Key.RETURN);
+  await email.sendKeys(testEmail, Key.RETURN);
+
+  await password.sendKeys('', Key.RETURN);
+  await password.sendKeys(testPasswordCorrect, Key.RETURN);
+
+  await submit.click();
+
+  driver.wait(until.urlIs('http://localhost:3000/login'));
+  await driver.get('http://localhost:3000/login');
+
+  await driver.wait(until.elementLocated(By.id('heading')), 20000);
   const heading = await driver.findElement(By.id('heading'));
   expect(heading).not.toEqual(null);
 }, 60000);
