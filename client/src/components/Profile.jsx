440baa4df1f83ff2efe6d41caf5eaee1d3e8634a
getinthedamnbox <getinthedamnbox@gmail.com>
2019-12-05 11:18:56 -0500

    Displayed user's profile picture

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,8 +1,8 @@
-/* globals fetch */
+/* globals btoa */
 
 import React, { Component } from 'react';
 import NavBar from './NavBar';
-import {getUser} from '../javascripts/userRequests';
+import { getUser } from '../javascripts/userRequests';
 
 class Profile extends Component {
   constructor(props) {
@@ -11,7 +11,7 @@ class Profile extends Component {
     this.state = {
       data: null,
       currentUser: props.currentUser,
-      isLoading: true
+      isLoading: true,
     };
   }
 
@@ -33,14 +33,14 @@ class Profile extends Component {
 
   render() {
     const { data, isLoading } = this.state;
-    if(isLoading){
-      return(
-        <div className='uk-cover-container uk-flex uk-flex-center uk-flex-middle'>
-        <h1>Wait a Sec...</h1>
+    if (isLoading) {
+      return (
+        <div className="uk-cover-container uk-flex uk-flex-center uk-flex-middle">
+          <h1>Wait a Sec...</h1>
         </div>
-      )
+      );
     }
-    else{
+
     return (
       <div>
         <NavBar />
@@ -48,27 +48,33 @@ class Profile extends Component {
           <div className="uk-container uk-container-small">
             <div className="uk-grid uk-margin-medium-bottom" uk-grid="true">
               <div className="uk-width-1-3 uk-flex uk-flex-middle uk-flex-center">
-                <img className="uk-border-pill" style={{ height: '150px', width: '150px' }} id="profile-image" src="" alt="" />
+                <img className="uk-border-pill" style={{ maxHeight: '150px', maxWidth: '150px' }} id="profile-image" src={`data:image/png;base64,${btoa(String.fromCharCode.apply(null, data.image.data))}`} alt="" />
               </div>
               <div className="uk-section uk-section-default uk-padding-small uk-margin-left">
                 <div className="uk-flex uk-margin-small-bottom uk-flex-row uk-flex-middle">
                   <h1 id="username" className="uk-text-light uk-margin-remove uk-heading-xsmall">{data.username}</h1>
-                  <a className="uk-button uk-button-default uk-margin-left" style={{height: '40px'}} href="/makePost">Create Post</a>
+                  <a className="uk-button uk-button-default uk-margin-left" style={{ height: '40px' }} href="/makePost">Create Post</a>
                 </div>
                 <ul className="uk-margin-remove" style={{ padding: '0px', listStyleType: 'none' }}>
                   <li className="uk-text-bold uk-margin-bottom uk-margin-right uk-float-left">
                     <span id="posts" className="uk-text-light">
-                      posts: {data.posts.length}
+                      posts:
+                      {' '}
+                      {data.posts.length}
                     </span>
                   </li>
                   <li className="uk-text-bold uk-margin-bottom uk-margin-left uk-margin-right uk-float-left">
                     <span id="followers" className="uk-text-light">
-                     followers: <a href='/follower'>{data.followers.length}</a>
+                     followers:
+                      {' '}
+                      <a href="/follower">{data.followers.length}</a>
                     </span>
                   </li>
                   <li className="uk-text-bold uk-margin-bottom uk-margin-left uk-float-left">
                     <span id="following" className="uk-text-light">
-                     following: <a href='/followee'>{data.followees.length}</a>
+                     following:
+                      {' '}
+                      <a href="/followee">{data.followees.length}</a>
                     </span>
                   </li>
                 </ul>
@@ -77,7 +83,7 @@ class Profile extends Component {
               </div>
               <div className="uk-container uk-container-small uk-margin-top">
                 <div id="recentPost" className="uk-child-width-1-2@m uk-align-center uk-background-default">
-                  {/* The most recent Post will be added here. */}
+                  {/* The most recent post will be added here. */}
                 </div>
               </div>
             </div>
@@ -85,7 +91,6 @@ class Profile extends Component {
         </div>
       </div>
     );
-    }
   }
 }
 
