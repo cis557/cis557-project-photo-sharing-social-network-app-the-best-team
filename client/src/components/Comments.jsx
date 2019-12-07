457f4c3c0877b27b931d67bcbfc9b3788e233ad7
tonytang731 <50162066+tonytang731@users.noreply.github.com>
2019-12-07 15:45:13 -0500

    Add Comments

--- client/src/components/Comments.jsx
+++ client/src/components/Comments.jsx
@@ -1,52 +1,42 @@
 /* globals fetch */
 import React, { Component } from 'react';
 import { api } from '../api';
+import PropTypes from 'prop-types';
+import { addComment, editComment, deleteComment} from '../javascripts/commentRequests';
 
 //function used to do the spliting, move the the backend
 
-let stringToArray = function(s){
-  const res = s.split(",")
-  return res
-}
+// let stringToArray = function(s){
+//   const res = s.split(",")
+//   return res
+// }
 
-const testData = {
-    postID: 'abcd',
-    comments: [{
-      mentions: ['Jeff'],
-      username: 'Nick1',
-      text: "I like it!"
-    },{
-      mentions: ['Jack'],
-      username: 'Nick2',
-      text: "I love it!"
-    }]
-  };
+// const testData = {
+//     postID: 'abcd',
+//     comments: [{
+//       mentions: ['Jeff'],
+//       username: 'Nick1',
+//       text: "I like it!"
+//     },{
+//       mentions: ['Jack'],
+//       username: 'Nick2',
+//       text: "I love it!"
+//     }]
+//   };
 
-class Comments extends Component {
+class Comment extends Component {
   constructor(props) {
     super(props);
-
+    
     this.state = { 
-      data: testData,
-      apiResponse: '' 
+      data: null,
+      isLoading: true,
+      currentUser: null,
     };
-
-    this.callAPI = this.callAPI.bind(this);
   }
 
-  componentDidMount() {
-    this.callAPI();
-  }
-
-  callAPI() {
-    fetch(`${api.url}/testAPI`)
-      .then((res) => res.text())
-      .then((res) => this.setState({ apiResponse: res }))
-      .catch((err) => err);
-  }
-
-  render() {
-    const {data} = this.state;
+render() {
+  const {data} = this.state;
     const comments = data.comments.map((comment) => { return(
         <article className="uk-comment-primary uk-visible-toggle uk-box-shadow-hover-small" tabindex="-1">
           <header className="uk-comment-header uk-position-relative">
@@ -63,16 +53,16 @@ class Comments extends Component {
   </article>
   )});
     return (
-      <div className="uk-container uk-container-small">
-        <textarea className="uk-textarea" rows="4" placeholder="Reply"></textarea>
-        <div className="uk-margin-small">
-        <textarea className="uk-textarea" rows="1" placeholder="Mentions (Separated with Commas)"></textarea>
-        </div>
-        <button type="submit" className="uk-button uk-button-primary uk-border uk-width-1-1" id="submit">Sumbit</button>
-        {comments}
-      </div>
+    <div className="uk-container uk-container-small">
+
+    </div>
     );
   }
 }
 
-export default Comments;
+Comment.propTypes = {
+  postid: PropTypes.string.isRequired,
+  currentUser: PropTypes.string.isRequired,
+};
+
+export default Comment;
--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types';
 import Heart from './Heart';
 import { getPost } from '../javascripts/postRequests';
 import loading from '../images/loading-post.svg';
+import Comment from '../components/Comments';
+import { addComment } from '../javascripts/commentRequests';
 
 class Post extends Component {
   constructor(props) {
@@ -13,8 +15,12 @@ class Post extends Component {
       postid: props.postid,
       isLoading: true,
       currentUser: props.currentUser,
-      data: null,
+      data: [],
+      text: '',
     };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
@@ -35,9 +41,35 @@ class Post extends Component {
       });
   }
 
+  handleChange(event) {
+    this.setState({ [event.target.id]: event.target.value });
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+
+    const { text } = this.state;
+    const { postId } = this.state;
+
+    addComment(
+      postId,
+      text,
+      )
+      .then((res) => {
+        if(res.ok){
+          console.log(text);
+        } else {
+          console.log(res)
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  
   render() {
     const { data, isLoading, currentUser } = this.state;
-
+    console.log(data.comments);
     if (isLoading) {
       return (
         <div className="uk-card uk-card-default uk-card-hover uk-align-center" uk-scrollspy="cls: uk-animation-slide-left; repeat: true">
@@ -53,7 +85,18 @@ class Post extends Component {
     // eslint-disable-next-line no-underscore-dangle
     const postid = data._id;
     let isLiked = false;
+    const comments = data.comments;
     const { likes } = data;
+
+    //New: Come back
+  //   const renderComments = [];
+    
+  //   comments.forEach((comment) => {
+  //   renderComments.push(<Comment postid={postid} commentid = {comment.comment_id} currentUser = {currentUser}/>);
+  // });
+
+    //END
+
     if (likes.indexOf(currentUser) !== -1) {
       isLiked = true;
     }
@@ -75,6 +118,7 @@ class Post extends Component {
             <p id="">{data.description}</p>
             <Heart isLiked={isLiked} postid={postid} />
           </div>
+          {renderComments}
         </div>
       );
     }
@@ -95,6 +139,10 @@ class Post extends Component {
           <p id="">{data.description}</p>
           <Heart isLiked={isLiked} postid={postid} />
         </div>
+        <form onSubmit={this.handleSubmit}>
+        <textarea onChange={this.handleChange} className="uk-textarea" rows="4" placeholder="Reply"></textarea>
+        <button type="submit" className="uk-button uk-button-primary uk-border uk-width-1-1" id="submit">Sumbit</button>
+      </form>
       </div>
     );
   }
