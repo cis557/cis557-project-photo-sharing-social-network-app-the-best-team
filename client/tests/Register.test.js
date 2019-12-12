d8966b3f26877753264f37cdf9e45ff8c80a9e33
tonytang731 <50162066+tonytang731@users.noreply.github.com>
2019-12-11 22:47:22 -0500

    React test setup

---
+++ client/tests/Register.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+test('Try tests', () => {
+  // Render a checkbox with label in the document
+  expect("Handsome").toEqual('Handsome');
+});
